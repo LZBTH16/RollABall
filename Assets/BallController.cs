8dd3578b385d9fce2b4dab9e9da3339ce2edf252
Liz <lizrd2016@gmail.com>
2025-01-14 21:41:07 -0800

    Add jump mechanic

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -4,16 +4,32 @@ public class BallController : MonoBehaviour
 {
     [SerializeField] private Rigidbody sphereRigidBody; // initializing the force component
     [SerializeField] private float ballSpeed = 2f; // to see the ball speed
+    // setting up jump capability
+    [SerializeField] private float jumpForce = 0.75f;
+    private bool isGrounded;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Calling the Start method");
+        sphereRigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionStay()
+    {
+        isGrounded = true;
     }
 
     public void MoveBall (Vector2 input)
     {
         Vector3 inputXZPlane = new(input.x, 0, input.y);
         sphereRigidBody.AddForce (inputXZPlane * ballSpeed);
+
+        // making it jump
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            sphereRigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+
+        }
     }
 }
